a1bfc6b38f12e8885c66673cad85e526d707eca9
KatyBeard <borodashkina.ekaterina@yandex.ru>
2022-12-05 23:14:57 +0500

    Added folder 'HW_Seminar2\task1'

---
+++ HW_Seminar2/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number / 100 == 0 || number / 100 > 9 || -number / 100 > 9) Console.WriteLine("Вы ввели не трехзначное число!!!");
+else if (number >= 0) Console.WriteLine($"Вторая цифра числа {number} --> {number / 10 % 10}");
+else Console.WriteLine($"Вторая цифра числа {number} --> {-number / 10 % 10}");
