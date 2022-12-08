edc0cb28b8abf54123897fd50bb55e42702a232e
KatyBeard <borodashkina.ekaterina@yandex.ru>
2022-12-08 16:45:48 +0500

    Added folder 'HW_Seminar3\task3'

---
+++ HW_Seminar3/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 1) {
+    int result = number;
+    Console.Write($"Таблица кубов чисел от {number} до 1: ");
+    while (result < 1)
+    {
+        Console.Write($"{result * result*result}, ");
+        result++;
+    }
+    Console.Write($"{result * result*result}");
+}
+else
+{
+    int result = 1;
+    Console.Write($"Таблица кубов чисел от 1 до {number}: ");
+    while (result < number)
+    {
+        Console.Write($"{result * result*result}, ");
+        result++;
+    }
+    Console.Write($"{result * result*result}");
+}
