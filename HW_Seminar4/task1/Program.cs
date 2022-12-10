46541d1d5757fc3ed35f64522c3be2f04c506eac
KatyBeard <borodashkina.ekaterina@yandex.ru>
2022-12-10 18:52:11 +0500

    Added folder 'HW_Seminar4\task1'

---
+++ HW_Seminar4/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите число: ");
+double number = double.Parse(Console.ReadLine()!);
+Console.Write("Введите степень: ");
+double degree = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number}^{degree} = {Exponentiation(number,degree)}");
+
+double Exponentiation (double number, double degree){
+    double exponentiation = Math.Pow(number, degree);
+    return exponentiation;
+}
