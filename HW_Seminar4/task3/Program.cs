12e2e881c1ab36d43a397c488772f3ac78fefc65
KatyBeard <borodashkina.ekaterina@yandex.ru>
2022-12-10 19:17:55 +0500

    Added folder 'HW_Seminar4\task3'

---
+++ HW_Seminar4/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите количество элементов в массиве: ");
+int count = int.Parse(Console.ReadLine()!);
+int[] array = new int[count];
+SetArray(array);
+Console.Write("Вы ввели слеующий массив: ");
+PrintArray(array);
+
+void SetArray (int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"Введите {i+1} элемент массива: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+void PrintArray (int[] array){
+    Console.Write("[");
+    for (int i = 0; i < array.Length-1; i++){
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length-1]}]");
+}
