6d6ec4e3042989420d788ccdda4c54dbab071893
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-01-03 17:58:10 +0500

    Added folder 'HW_Seminar5\task2'

---
+++ HW_Seminar5/task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+int[] array = GetArray(5, -10, 10);
+PrintArray(array);
+Console.Write(" -> ");
+
+int SumOddElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+Console.Write($"Сумма элементов, стоящих на нечётных позициях равна {SumOddElement(array)}");
