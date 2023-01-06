8f3c9dd00cf6f2b9b0dc8cb08e7b6635112ea559
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-01-06 17:34:21 +0500

    Added folder 'HW_Seminar6\task1'

---
+++ HW_Seminar6/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M
+// чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+
+Console.Write("Сколько чисел вы хотите ввести? ");
+int M = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(M);
+int positiveNumber = PositiveNumbers(array);
+
+Console.Write($"Вы ввели {positiveNumber} чисел(-ла) больше 0");
+
+int[] GetArray(int size){
+    int[] res = new int[size];
+    for(int i=0;i<size;i++){
+        Console.Write($"Введите {i+1} число: ");
+        res[i] = int.Parse(Console.ReadLine()!);
+    }
+    return res;
+}
+
+int PositiveNumbers(int[] array){
+    int res = 0;
+    for(int i = 0; i<array.Length; i++){
+        if (array[i]>0) res++;
+    }
+    return res;
+}
