e918acd134928fb608a2a1f6f18e00b33b14764d
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-01-27 01:25:44 +0500

    Added folder 'HW_Seminar8\task4'

---
+++ HW_Seminar8/task4/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся
+// двузначных чисел. Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+
+int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+{
+    int[,,] result = new int[x, y, z];
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+                result[i, j, k] = CheckNumber(result, result[i, j, k]);
+            }
+        }
+    }
+    return result;
+}
+
+int CheckNumber(int[,,] array, int number)
+{
+    int result = number;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == result && result != 99) return CheckNumber(array, result + 1);
+                else if (array[i, j, k] == result && result == 99) return CheckNumber(array, 10);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Console.Write($"{inArray[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите высоту массива: ");
+int height = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+Console.WriteLine("Сформерованный трехмерный массив: ");
+int[,,] array = GetArray(rows, columns, height, 10, 99);
+PrintArray(array);
