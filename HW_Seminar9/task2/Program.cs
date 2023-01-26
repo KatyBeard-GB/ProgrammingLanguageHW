0f799b2b06cdf74d2604e57ccbde15c43d5a91e7
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-01-27 04:49:09 +0500

    Added folder 'HW_Seminar9\task2'

---
+++ HW_Seminar9/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M: ");
+int numberM = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите значение N: ");
+int numberN = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+sum = SumNumbers(numberM,numberN,sum);
+Console.WriteLine($"Сумма натуральных элементов от {numberM} до {numberN} равна {sum}.");
+
+int SumNumbers(int number, int maxNumber, int result){
+    if (number == maxNumber+1) return result;
+    else return SumNumbers(number+1,maxNumber,result+number);
+}
