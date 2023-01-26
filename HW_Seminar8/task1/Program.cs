ef0bfccf3d3b0962bc218700e475e01647118fd9
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-01-27 00:01:00 +0500

    Added folder 'HW_Seminar8\task1'

---
+++ HW_Seminar8/task1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrangeArray(int[,] array){
+    int[,] result = new int[array.GetLength(0),array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           result[i,j] = array[i,j]; 
+        }
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           for (int k = 0; k < array.GetLength(1); k++)
+        {
+            if (result[i,j] > result[i,k]) {
+                int count = result[i,j];
+                result[i,j] = result[i,k];
+                result[i,k] = count;
+            }
+        }
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+int[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+Console.WriteLine();
+
+int[,] arrangeArray = ArrangeArray(array);
+PrintArray(arrangeArray);
